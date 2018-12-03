scriptotek/alma-slipsomat

commit 0c605f3b1fb101d70d3cd5cb6a35f0ab09f3760f

Workaround for #30
I'm not sure if we can rely on the html window always being the latest
one, but let's try.

slipsomat/slipsomat.py
@@ -550,24 +550,23 @@ class TestPage(object):
         time.sleep(1)
 
         # Take a screenshot
-        found_win = False
-        for handle in self.worker.driver.window_handles:
-            self.worker.driver.switch_to_window(handle)
-            if 'beanContentParam=htmlContent' in self.worker.driver.current_url:
-                self.worker.driver.set_window_size(
-                    self.worker.config.get('screenshot', 'width'), 600)
-                with open(html_path, 'w+b') as html_file:
-                    html_file.write(self.worker.driver.page_source.encode('utf-8'))
-                print('Saved output: %s' % html_path)
-                if self.worker.driver.save_screenshot(png_path):
-                    print('Saved screenshot: %s' % png_path)
-                else:
-                    print('Failed to save screenshot')
-                found_win = True
-                break
+        self.worker.driver.switch_to_window(self.worker.driver.window_handles[-1])
 
-        if not found_win:
-            print(Fore.RED + 'ERROR: Failed to produce output!' + Fore.RESET)
+            # if 2 == 1 and 'beanContentParam=htmlContent' in self.worker.driver.current_url:
+        self.worker.driver.set_window_size(
+            self.worker.config.get('screenshot', 'width'),
+            600
+        )
+        with open(html_path, 'w+b') as html_file:
+            html_file.write(self.worker.driver.page_source.encode('utf-8'))
+        print('Saved output: %s' % html_path)
+        if self.worker.driver.save_screenshot(png_path):
+            print('Saved screenshot: %s' % png_path)
+        else:
+            print('Failed to save screenshot')
+
+        # if not found_win:
+        #     print(Fore.RED + 'ERROR: Failed to produce output!' + Fore.RESET)
         self.worker.driver.switch_to_window(cwh)
         tmp.close()
 

commit 2fe2c178c8ae748f8ce6862f2ff9a316ad8b3fa1

Workaround for screenshot from wrong window
Had a suspicion that the workaround introduced in 0c605f3 was too
simple. Still haven't found a *proper* way to do this. #30

slipsomat/slipsomat.py
@@ -552,6 +552,11 @@ class TestPage(object):
 
         # Take a screenshot
         self.worker.driver.switch_to_window(self.worker.driver.window_handles[-1])
+
+        if self.worker.driver.page_source.startswith('<xsl'):
+            # Wrong window, try next one
+            self.worker.driver.switch_to_window(self.worker.driver.window_handles[-2])
+
         # GitHub: #30  -> if 'beanContentParam=htmlContent' in self.worker.driver.current_url:
         self.worker.driver.set_window_size(
             self.worker.config.get('screenshot', 'width'),